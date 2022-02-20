6a35b878ee85388aa723f4a45b2203ba35e44fad
Josuah Demangeon <me@josuah.net>
2022-02-20 18:39:11 +0100

    clean the SDK directory objects as well on "make clean"

--- gpio.h
+++ gpio.h
@@ -12,21 +12,21 @@ struct zmcu_gpio {
 
 	/* 0x00: Port control register */
 	uint64_t volatile CTL;
-#define GPIO_CTL(pin, x)		((uint64_t)(x) << ((pin) * 4))
-#define GPIO_CTL_MASK(pin)		GPIO_CTL(pin, B00000011)
-#define GPIO_CTL_INPUT_ANALOG		B00000000
-#define GPIO_CTL_INPUT_FLOATING		B00000001
-#define GPIO_CTL_INPUT_PULL_UP_DOWN	B00000010
-#define GPIO_CTL_OUTPUT_PUSH_PULL	B00000000
-#define GPIO_CTL_OUTPUT_OPEN_DRAIN	B00000001
-#define GPIO_CTL_ALTFN_PUSH_PULL	B00000010
-#define GPIO_CTL_ALTFN_OPEN_DRAIN	B00000011
-#define GPIO_MD(pin, x)			((uint64_t)(x) << ((pin) * 4 + 2))
-#define GPIO_MD_MASK(pin)		GPIO_MD(pin, B00000011)
-#define GPIO_MD_INPUT			B00000000
-#define GPIO_MD_OUTPUT_10_MHZ		B00000001
-#define GPIO_MD_OUTPUT_2_MHZ		B00000010
-#define GPIO_MD_OUTPUT_50_MHZ		B00000011
+#define GPIO_CTL(pin, x)			((uint64_t)(x) << ((pin) * 4))
+#define GPIO_CTL_MASK(pin)			GPIO_CTL(pin, B00000011)
+#define GPIO_CTL_INPUT_ANALOG			B00000000
+#define GPIO_CTL_INPUT_FLOATING			B00000001
+#define GPIO_CTL_INPUT_PULL_UP_DOWN		B00000010
+#define GPIO_CTL_OUTPUT_PUSH_PULL		B00000000
+#define GPIO_CTL_OUTPUT_OPEN_DRAIN		B00000001
+#define GPIO_CTL_ALTFN_PUSH_PULL		B00000010
+#define GPIO_CTL_ALTFN_OPEN_DRAIN		B00000011
+#define GPIO_MD(pin, x)				((uint64_t)(x) << ((pin) * 4 + 2))
+#define GPIO_MD_MASK(pin)			GPIO_MD(pin, B00000011)
+#define GPIO_MD_INPUT				B00000000
+#define GPIO_MD_OUTPUT_10_MHZ			B00000001
+#define GPIO_MD_OUTPUT_2_MHZ			B00000010
+#define GPIO_MD_OUTPUT_50_MHZ			B00000011
 
 	/* 0x08: Port input status register */
 	uint32_t volatile ISTAT;
@@ -36,15 +36,15 @@ struct zmcu_gpio {
 
 	/* 0x10: Port bit operate register */
 	uint32_t volatile BOP;
-#define GPIO_BOP_SET(x)			(((x) & 0xFFFF) << 0)
-#define GPIO_BOP_CLR(x)			(((x) & 0xFFFF) << 16)
+#define GPIO_BOP_SET(x)				(((x) & 0xFFFF) << 0)
+#define GPIO_BOP_CLR(x)				(((x) & 0xFFFF) << 16)
 
 	/* 0x14: Port bit clear register */
 	uint32_t volatile BC;
 
 	/* 0x18: Port configuration lock register */
 	uint32_t volatile LOCK;
-#define GPIO_LKK			(1u << 16)
+#define GPIO_LKK				(1u << 16)
 
 };
 
@@ -134,6 +134,6 @@ gpio_mode_altfn_open_drain(struct zmcu_gpio *gpio, uint8_t pin, uint32_t speed)
 	gpio_mode(gpio, pin, speed, GPIO_CTL_ALTFN_OPEN_DRAIN);
 }
 
-#define GPIO_SPEED_2_MHZ		GPIO_MD_OUTPUT_10_MHZ
-#define GPIO_SPEED_10_MHZ		GPIO_MD_OUTPUT_2_MHZ
-#define GPIO_SPEED_50_MHZ		GPIO_MD_OUTPUT_50_MHZ
+#define GPIO_SPEED_2_MHZ			GPIO_MD_OUTPUT_10_MHZ
+#define GPIO_SPEED_10_MHZ			GPIO_MD_OUTPUT_2_MHZ
+#define GPIO_SPEED_50_MHZ			GPIO_MD_OUTPUT_50_MHZ
--- usart.h
+++ usart.h
@@ -12,79 +12,79 @@ struct zmcu_usart {
 
 	/* 0x00: Status register */
 	uint32_t volatile STAT;
-#define USART_STAT_PERR			(1u << 0)
-#define USART_STAT_FERR			(1u << 1)
-#define USART_STAT_NERR			(1u << 2)
-#define USART_STAT_ORERR		(1u << 3)
-#define USART_STAT_IDLEF		(1u << 4)
-#define USART_STAT_RBNE			(1u << 5)
-#define USART_STAT_TC			(1u << 6)
-#define USART_STAT_TBE			(1u << 7)
-#define USART_STAT_LBDF			(1u << 8)
-#define USART_STAT_CTSF			(1u << 9)
+#define USART_STAT_PERR				(1u << 0)
+#define USART_STAT_FERR				(1u << 1)
+#define USART_STAT_NERR				(1u << 2)
+#define USART_STAT_ORERR			(1u << 3)
+#define USART_STAT_IDLEF			(1u << 4)
+#define USART_STAT_RBNE				(1u << 5)
+#define USART_STAT_TC				(1u << 6)
+#define USART_STAT_TBE				(1u << 7)
+#define USART_STAT_LBDF				(1u << 8)
+#define USART_STAT_CTSF				(1u << 9)
 
 	/* 0x04: Data register */
 	uint32_t volatile DATA;
 
 	/* 0x08: Baud rate register */
 	uint32_t volatile BAUD;
-#define USART_BAUD_INTDIV(x)		((x) << 4)
-#define USART_BAUD_INTDIV_MASK		USART_BAUD_INTDIV(0xFFF)
-#define USART_BAUD_FRADIV(x)		((x) << 0)
-#define USART_BAUD_FRADIV_MASK		USART_BAUD_FRADIV(0xF)
+#define USART_BAUD_INTDIV(x)			((x) << 4)
+#define USART_BAUD_INTDIV_MASK			USART_BAUD_INTDIV(0xFFF)
+#define USART_BAUD_FRADIV(x)			((x) << 0)
+#define USART_BAUD_FRADIV_MASK			USART_BAUD_FRADIV(0xF)
 
 	/* 0x0C: Control register 0 */
 	uint32_t volatile CTL0;
-#define USART_CTL0_SBKCMD		(1u << 0)
-#define USART_CTL0_RWU			(1u << 1)
-#define USART_CTL0_REN			(1u << 2)
-#define USART_CTL0_TEN			(1u << 3)
-#define USART_CTL0_IDLEIE		(1u << 4)
-#define USART_CTL0_RBNEIE		(1u << 5)
-#define USART_CTL0_TCIE			(1u << 6)
-#define USART_CTL0_TBEIE		(1u << 7)
-#define USART_CTL0_PERRIE		(1u << 8)
-#define USART_CTL0_PM			(1u << 9)
-#define USART_CTL0_PCEN			(1u << 10)
-#define USART_CTL0_WM			(1u << 11)
-#define USART_CTL0_WL			(1u << 12)
-#define USART_CTL0_UEN			(1u << 13)
+#define USART_CTL0_SBKCMD			(1u << 0)
+#define USART_CTL0_RWU				(1u << 1)
+#define USART_CTL0_REN				(1u << 2)
+#define USART_CTL0_TEN				(1u << 3)
+#define USART_CTL0_IDLEIE			(1u << 4)
+#define USART_CTL0_RBNEIE			(1u << 5)
+#define USART_CTL0_TCIE				(1u << 6)
+#define USART_CTL0_TBEIE			(1u << 7)
+#define USART_CTL0_PERRIE			(1u << 8)
+#define USART_CTL0_PM				(1u << 9)
+#define USART_CTL0_PCEN				(1u << 10)
+#define USART_CTL0_WM				(1u << 11)
+#define USART_CTL0_WL				(1u << 12)
+#define USART_CTL0_UEN				(1u << 13)
 
 	/* 0x10: Control register 1 */
 	uint32_t volatile CTL1;
-#define USART_CTL1_ADDR(x)		((x) << 0)
-#define USART_CTL1_LBLEN		(1u << 5)
-#define USART_CTL1_LBDIE		(1u << 6)
-#define USART_CTL1_CLEN			(1u << 8)
-#define USART_CTL1_CPH			(1u << 9)
-#define USART_CTL1_CPL			(1u << 10)
-#define USART_CTL1_CKEN			(1u << 11)
-#define USART_CTL1_STB(x)		((x) << 12)
-#define USART_CTL1_STB_MASK		USART_CTL1_STB(B00000011)
-#define USART_CTL1_STB_1		B00000000
-#define USART_CTL1_STB_0_5		B00000001
-#define USART_CTL1_STB_2		B00000010
-#define USART_CTL1_STB_1_5		B00000011
-#define USART_CTL1_LMEN			(1u << 14)
+#define USART_CTL1_ADDR(x)			((x) << 0)
+#define USART_CTL1_LBLEN			(1u << 5)
+#define USART_CTL1_LBDIE			(1u << 6)
+#define USART_CTL1_CLEN				(1u << 8)
+#define USART_CTL1_CPH				(1u << 9)
+#define USART_CTL1_CPL				(1u << 10)
+#define USART_CTL1_CKEN				(1u << 11)
+#define USART_CTL1_STB(x)			((x) << 12)
+#define USART_CTL1_STB_MASK			USART_CTL1_STB(B00000011)
+#define USART_CTL1_STB_1			B00000000
+#define USART_CTL1_STB_0_5			B00000001
+#define USART_CTL1_STB_2			B00000010
+#define USART_CTL1_STB_1_5			B00000011
+#define USART_CTL1_LMEN				(1u << 14)
 
 	/* 0x14: Control register 2 */
 	uint32_t volatile CTL2;
-#define USART_CTL2_ERRIE		(1u << 0)
-#define USART_CTL2_IREN			(1u << 1)
-#define USART_CTL2_IRLP			(1u << 2)
-#define USART_CTL2_HDEN			(1u << 3)
-#define USART_CTL2_NKEN			(1u << 4)
-#define USART_CTL2_SCEN			(1u << 5)
-#define USART_CTL2_DENR			(1u << 6)
-#define USART_CTL2_DENT			(1u << 7)
-#define USART_CTL2_RTSEN		(1u << 8)
-#define USART_CTL2_CTSEN		(1u << 9)
-#define USART_CTL2_CTSIE		(1u << 10)
+#define USART_CTL2_ERRIE			(1u << 0)
+#define USART_CTL2_IREN				(1u << 1)
+#define USART_CTL2_IRLP				(1u << 2)
+#define USART_CTL2_HDEN				(1u << 3)
+#define USART_CTL2_NKEN				(1u << 4)
+#define USART_CTL2_SCEN				(1u << 5)
+#define USART_CTL2_DENR				(1u << 6)
+#define USART_CTL2_DENT				(1u << 7)
+#define USART_CTL2_RTSEN			(1u << 8)
+#define USART_CTL2_CTSEN			(1u << 9)
+#define USART_CTL2_CTSIE			(1u << 10)
 
 	/* 0x18: Guard time and prescaler register */
 	uint32_t volatile GP;
-#define USART_GP_PSC(x)			((x) << 0)
-#define USART_GP_GUAT(x)		((x) << 8)
+#define USART_GP_PSC(x)				((x) << 0)
+#define USART_GP_GUAT(x)			((x) << 8)
 
 };
 
@@ -96,10 +96,10 @@ void usart_set_word_length(struct zmcu_usart *usart, uint8_t len);
 
 /* set the number of stop bits, 5 for 0.5, 10 for 1.0, 15 for 1.5... */
 void usart_set_stop_bits(struct zmcu_usart *usart, uint8_t stop_bits);
-#define USART_STOP_BITS_0_5		USART_CTL1_STB_0_5
-#define USART_STOP_BITS_1_0		USART_CTL1_STB_1_0
-#define USART_STOP_BITS_1_5		USART_CTL1_STB_1_5
-#define USART_STOP_BITS_2_0		USART_CTL1_STB_2_0
+#define USART_STOP_BITS_0_5			USART_CTL1_STB_0_5
+#define USART_STOP_BITS_1_0			USART_CTL1_STB_1_0
+#define USART_STOP_BITS_1_5			USART_CTL1_STB_1_5
+#define USART_STOP_BITS_2_0			USART_CTL1_STB_2_0
 
 /* enable parity checking with an odd parity mode */
 void usart_mode_parity_odd(struct zmcu_usart *usart);
